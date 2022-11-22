cafc4b47e2a9ce3b7745f78b6bfb5dcb85621690
archana53 <archanakutumbaka7@gmail.com>
2022-11-21 19:20:41 -0500

    added data loading

---
+++ data_loading.py
@@ -0,0 +1,61 @@
+from torchvision import datasets,transforms
+from torch.utils.data import Dataset
+import numpy as np
+
+config = {'size': 32, 'channels': 1, 'classes': 10}
+
+def permutate_image_pixels(image, permutation):
+    '''Permutate the pixels of an image according to [permutation].
+    [image]         3D-tensor containing the image
+    [permutation]   <ndarray> of pixel-indeces in their new order'''
+
+    if permutation is None:
+        return image
+    else:
+        c, h, w = image.size()
+        image = image.view(c, -1)
+        image = image[:, permutation]  #--> same permutation for each channel
+        image = image.view(c, h, w)
+        return image
+
+class TransformedDataset(Dataset):
+    '''To modify an existing dataset with a transform.
+    This is useful for creating different permutations of MNIST without loading the data multiple times.'''
+
+    def __init__(self, original_dataset, transform=None, target_transform=None):
+        super().__init__()
+        self.dataset = original_dataset
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        (input, target) = self.dataset[index]
+        if self.transform:
+            input = self.transform(input)
+        if self.target_transform:
+            target = self.target_transform(target)
+        return (input, target)
+
+def permute_train_test_data(mnist_trainset, mnist_testset) :
+        # get train and test datasets
+        # generate pixel-permutations
+        permutations = [np.random.permutation(32**2) for _ in range(10)]
+        # specify transformed datasets per context
+        train_datasets = []
+        test_datasets = []
+        for perm in enumerate(permutations):
+            target_transform = None
+            train_datasets.append(TransformedDataset(
+                mnist_trainset, transform=transforms.Lambda(lambda x, p=perm: permutate_image_pixels(x, p)),
+                target_transform=target_transform
+            ))
+            test_datasets.append(TransformedDataset(
+                mnist_testset, transform=transforms.Lambda(lambda x, p=perm: permutate_image_pixels(x, p)),
+                target_transform=target_transform
+            ))
+
+        return train_datasets, test_datasets
+
